28b5182fd36fea011e128d5ad19c75a1ee26635c
Qian Sun <qians@google.com>
2017-01-06 10:32:51 -0800

    Define interface for client.

---
+++ include/client.h
@@ -0,0 +1,100 @@
+/* Copyright 2016 Google Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef MIXERCLIENT_CLIENT_H
+#define MIXERCLIENT_CLIENT_H
+
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "google/api/servicecontrol/v1/service_controller.pb.h"
+#include "google/protobuf/stubs/status.h"
+#include "options.h"
+
+namespace google {
+namespace mixer_client {
+
+// Defines a function prototype used when an asynchronous transport call
+// is completed.
+using TransportDoneFunc =
+    std::function<void(const ::google::protobuf::util::Status&)>;
+
+// Defines a function prototype to make an asynchronous Check call to
+// the mixer server.
+using TransportCheckFunc = std::function<void(
+    const ::istio::mixer::v1::CheckRequest& request,
+    ::istio::mixer::v1::CheckResponse* response, TransportDoneFunc on_done)>;
+
+// Defines a function prototype to make an asynchronous Report call to
+// the mixer server.
+using TransportReportFunc = std::function<void(
+    const ::istio::mixer::v1::ReportRequest& request,
+    ::istio::mixer::v1::ReportResponse* response, TransportDoneFunc on_done)>;
+
+// Defines the options to create an instance of MixerClient interface.
+struct MixerClientOptions {
+  // Default constructor with default values.
+  MixerClientOptions() {}
+
+  // Constructor with specified option values.
+  MixerClientOptions(const CheckOptions& check_options,
+                     const ReportOptions& report_options)
+      : check_options(check_options), report_options(report_options) {}
+
+  // Check aggregation options.
+  CheckOptions check_options;
+
+  // Report aggregation options.
+  ReportOptions report_options;
+
+  // Transport functions are used to send request to mixer server.
+  // It can be implemented many ways based on the environments.
+  // If not provided, the GRPC transport will be used.
+  TransportCheckFunc check_transport;
+  TransportReportFunc report_transport;
+};
+
+class MixerClient {
+ public:
+  using DoneCallback =
+      std::function<void(const ::google::protobuf::util::Status&)>;
+
+  // Destructor
+  virtual ~MixerClient() {}
+
+  // A check call with provided per_request transport function.
+  // Only some special platforms may need to use this function.
+  // It allows caller to pass in a per_request transport function.
+  virtual void Check(const ::istio::mixer::v1::CheckRequest& check_request,
+                     ::istio::mixer::v1::CheckResponse* check_response,
+                     DoneCallback on_check_done,
+                     TransportCheckFunc check_transport) = 0;
+
+  // A report call with provided per_request transport function.
+  // Only some special platforms may need to use this function.
+  // It allows callers to pass in a per_request transport function.
+  virtual void Report(const ::istio::mixer::v1::ReportResponse& report_request,
+                      ::istio::mixer::v1::ReportResponse* report_response,
+                      DoneCallback on_report_done,
+                      TransportReportFunc report_transport) = 0;
+};
+
+// Creates a MixerClient object.
+std::unique_ptr<MixerClient> CreateMixerClient(
+    const std::string& service_name, const std::string& service_config_id,
+    MixerClientOptions& options);
+
+}  // namespace mixer_client
+}  // namespace google
+
+#endif  // MIXERCLIENT_CLIENT_H
