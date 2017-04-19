cc144f7119a114a0bf956681daf71477944a1324
Wayne Zhang <qiwzhang@google.com>
2017-04-19 09:48:05 -0700

    fix format

--- prefetch/circular_queue_test.cc
+++ prefetch/circular_queue_test.cc
@@ -20,8 +20,7 @@ namespace istio {
 namespace mixer_client {
 namespace {
 
-void ASSERT_RESULT(CircularQueue<int>& q,
-                   const std::vector<int>& expected) {
+void ASSERT_RESULT(CircularQueue<int>& q, const std::vector<int>& expected) {
   std::vector<int> v;
   q.Iterate([&](int& i) -> bool {
     v.push_back(i);
